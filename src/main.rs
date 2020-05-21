47e257490b45064975607d99da3e5ebe12d0c9c6
macdrien <adrien.bouyssou@gmail.com>
2020-05-21 13:10:55 +0200

    [UPD] Remove Structs and debug command reading

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
-use std::vec::Vec;
 use std::io;
 
 fn main() {
     println!("Welcome into your TODO app!");
     
     loop {
-        print!("Please enter your command : ");
-        
-            let mut command = String::new();
-        
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read the line");
+        println!("Please input you command :");
 
-        println!("You wrote : {}", command);
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).expect("Failed to read lines");
+
+        command.truncate(command.len() - 1);
 
         if command == "exit" { 
             println!("See you later!");
             break;
         }
     }
-}
-
-#[derive(Debug)]
-struct Todo {
-    title: String,
-}
-
-#[derive(Debug)]
-struct TodoList {
-    list: Vec<Todo>,
 }
